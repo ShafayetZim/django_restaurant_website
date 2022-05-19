77cd2d05a889c6d911bb7d4482876216e0804c13
ShafayetZim <shafayet.zim@northsouth.edu>
2022-05-19 18:23:25 +0600

    tracking form added

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Product
+from .models import Product, OrderUpdate
 
 
 class ProductCreateForm(forms.ModelForm):
@@ -14,5 +14,33 @@ class ProductCreateForm(forms.ModelForm):
             'desc': forms.TextInput(attrs={'class': 'form-control'}),
             'pub_date': forms.DateInput(attrs={'type': 'date'}),
             'image': forms.FileInput(attrs={'class': 'rounded_list'}),
+
+        }
+
+
+class ProductUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = (
+            'product_name', 'category', 'price', 'desc', 'pub_date', 'image',)
+        widgets = {
+            'product_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'category': forms.Select(attrs={'class': 'form-control'}),
+            'price': forms.NumberInput(attrs={'class': 'form-control'}),
+            'desc': forms.TextInput(attrs={'class': 'form-control'}),
+            'pub_date': forms.DateInput(attrs={'type': 'date'}),
+            'image': forms.FileInput(attrs={'class': 'rounded_list'}),
+
+        }
+
+
+class TrackingForm(forms.ModelForm):
+    class Meta:
+        model = OrderUpdate
+        fields = (
+            'order_id', 'update_desc')
+        widgets = {
+            'order_id': forms.TextInput(attrs={'class': 'form-control', 'readonly': 'readonly'}),
+            'update_desc': forms.TextInput(attrs={'class': 'form-control'}),
 
         }
